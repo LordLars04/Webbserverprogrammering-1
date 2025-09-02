7461ae7dccb9ea651bfec65e79223560dcb23e79
larssondde <larssondde@gmail.com>
2025-09-02 11:39:59 +0000

    [Add] router to save messages

---
+++ Lektion-1/Klient/index.js
@@ -0,0 +1,2 @@
+const form = document.querySelectior(".input-form")
+
--- Lektion-1/Server/app.mjs
+++ Lektion-1/Server/app.mjs
@@ -1,15 +1,40 @@
 import express from "express"
 import cors from "cors"
+import fs from "fs"
 
 const app = express()
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-})
+
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+const saveMessage = () => {
+  fs.writeFileSync(fileURLToPath, JSON.stringify(messages))
+}
+
+app.post("/messages",(req,res) => {
+  const {name, message} = req.body
+
+ 
+  saveMessage(message)
+
+  try { 
+    const newMessage =  {
+      name,
+      message,
+      timestamp: new Date().toISOString()
+    }
+    saveMessage(message)
+    res.status(201).json("Saved successfully")
+    }
+  catch (error) {
+    console.log ("Error: ", error)
+    res.status(500).json("Internal server errror");
+  }
+  
+})
 
 export default app 
